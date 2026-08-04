7cfbd67ebbc3b7beea2d12c268d3284bd66f4b9c
Your Name <parthlumos21@gmail.com>
2026-08-04 16:46:18 +0530

    Remove obsolete backend folder to fix confusion

--- backend/update_config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { EXCEL_FIELD_DEFS, EXCEL_DEPARTMENTS } = require('./src/constants/excelFormat');
-
-mongoose.connect('mongodb://localhost:27017/swati_switchgears_db').then(async () => {
-  const Config = mongoose.model('Config', new mongoose.Schema({key: String}, {strict: false}));
-  await Config.updateOne({key: 'system_config'}, {$set: {fieldDefs: EXCEL_FIELD_DEFS, departments: EXCEL_DEPARTMENTS}});
-  console.log('Updated config in DB with departments and fields');
-  process.exit(0);
-}).catch(console.error);
